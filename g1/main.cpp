1e4d7c498c818f1a76d5e0f899acb07a6e004f7e
j heinz <justinpaulheintz@gmail.com>
2021-09-21 16:45:20 -0400

    link

--- g1/main.cpp
+++ g1/main.cpp
@@ -135,7 +135,7 @@ vector<int> attrs = {3,2};
 void init() {
     /*start texture*/
     int width, height, nrChannels;
-    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("link.png", &width, &height, &nrChannels, 0);
 
     glGenTextures(1, &texture);
 
@@ -146,7 +146,7 @@ void init() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
     /*end texture*/
